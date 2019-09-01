e7ce9e3f7549f114205bf09407d78c2ab132b6df
scott <scottw@swcreative.com.au>
2019-09-01 12:36:39 +1000

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,14 @@
+
+//Confirm JS file is connected to the index.html file
+console.log("Up and running!");
+
+
+//create four variables that simulate the cards in the game
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+//Use console.log to display the value of two cards picked
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardFour);
